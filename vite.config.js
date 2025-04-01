d12d2230caaf3b92256e96852850bcb16806b76a
Chris <Chris08tm@hotmail.com>
2025-03-31 21:46:48 -0500

    add domain

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
       "a399-2600-1702-5086-1f1f-8c4e-2b3f-7088-2c5f.ngrok-free.app", // Specific ngrok URL
       "localhost", // Localhost (for local requests)
       "0.0.0.0", // Accept connections from any IP
+      "domino.alldadefinish.com",
     ],
   },
 });
